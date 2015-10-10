cf66306f96a49ad5a87cfcd9d8e666a815ea236b
Kate <katerebekah@gmail.com>
2015-10-10 13:11:20 -0500

    register page

---
+++ javascripts/register.js
@@ -0,0 +1,22 @@
+define(["jquery"], function($) {
+	var registerRef = new Firebase("https://petdatingsite.firebaseio.com/");
+	
+	$('#registerButton').click(function(e) {
+			e.preventDefault();
+	    var form = {
+	    	firstName: $('#firstName').val(),
+	    	lastName: $('#lastName').val(),
+	    	email: $('#email').val(),
+	    	password: $('#password').val()
+	    }
+
+	    registerRef.createUser(form, function(error, userData){
+	    	if (error) {
+	    		console.log(error);
+	    	}
+	    	console.log(userData);
+	    });
+	});
+	
+});
+
