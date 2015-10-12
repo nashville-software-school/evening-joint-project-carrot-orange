2675cfa13312f4c4a287840be107cedc689c1f13
Alejandro Gutierrez <alejandrogutierrez17@Alejandros-MBP.hsd1.tn.comcast.net>
2015-10-11 21:44:16 -0500

    posted correct object

--- javascripts/profile.js
+++ javascripts/profile.js
@@ -1,20 +1,30 @@
 define(["jquery"], function($) {
-    var firebaseRef = new Firebase("https://petdatingsite.firebaseio.com/");
+    var firebaseRef = new Firebase("https://petdatingsite.firebaseio.com/profiles");
+
+    var authData = firebaseRef.getAuth();
+
+    if(authData) {
+        console.log("Authenticated user with uid:", authData);
+    }
+
     $("#submitButton").click(function(e){
 
          e.preventDefault();
 
-         var petForm =  {
-            "name": $("#name").val(),
-            "age" : $("#age").val(),
-            "location" : $("#location").val(),
-            "breed" : $("#breed").val(),
-            "species" : $("#species").val(),
-            "profilePic": $("#profilePic").val(),
-            "ownerName" : $("#ownerName").val(),
-            "orientation" : $("#orientation").val(),
-            "gender" : $("#gender").val()
-        };
+         var petForm =
+            {
+              "name": $("#name").val(),
+              "age" : $("#age").val(),
+              "location" : $("#location").val(),
+              "breed" : $("#breed").val(),
+              "species" : $("#species").val(),
+              "profilePic": $("#profilePic").val(),
+              "ownerName" : $("#ownerName").val(),
+              "orientation" : $("#orientation").val(),
+              "gender" : $("#gender").val()
+            };
+
+                           // "userID" : authData.uid
 
          var form = firebaseRef.push();
          form.set(petForm, function(error, data) {
