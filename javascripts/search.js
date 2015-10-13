2d15a6d1d46187bcc0b789c264a684f99f79d0bf
Kate <katerebekah@gmail.com>
2015-10-12 19:27:34 -0500

    removing infinite loop of doom

--- javascripts/search.js
+++ javascripts/search.js
@@ -1,9 +1,6 @@
 define(["jquery"], function($) {
     var loginRef = new Firebase("https://petdatingsite.firebaseio.com/");
     var userAuth = loginRef.getAuth();
-    if (!userAuth){
-    	document.location = "/";
-    }
     $(document).on('click', '.like', function(e) {
         var petUserId = this.id;
         var updateURL = new Firebase("https://petdatingsite.firebaseio.com/profiles/" + petUserId + '/likes');
